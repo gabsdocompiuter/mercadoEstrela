d791d0f5fb949ecbe043a92bc5527f8f2c396767
Gabriel Monteiro <gmonteeeiro@gmail.com>
2019-06-11 11:59:52 -0300

    Altera os ícones do sistema

---
+++ android/app/src/main/res/round.bat
@@ -0,0 +1,6 @@
+DEL *round.png /s
+COPY mipmap-hdpi\ic_launcher.png    mipmap-hdpi\ic_launcher_round.png
+COPY mipmap-mdpi\ic_launcher.png    mipmap-mdpi\ic_launcher_round.png
+COPY mipmap-xhdpi\ic_launcher.png   mipmap-xhdpi\ic_launcher_round.png
+COPY mipmap-xxhdpi\ic_launcher.png  mipmap-xxhdpi\ic_launcher_round.png
+COPY mipmap-xxxhdpi\ic_launcher.png mipmap-xxxhdpi\ic_launcher_round.png
